8efc3728c4a8397f67f8caf454b0bde08357161c
Ben Cackett <ben.cackett@stud.fh-campuswien.ac.at>
2025-05-08 18:07:50 +0200

    Push unversioned files

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/WatchlistCell.java
@@ -0,0 +1,65 @@
+package at.ac.fhcampuswien.fhmdb.ui;
+
+import at.ac.fhcampuswien.fhmdb.ClickEventHandler;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.jfoenix.controls.JFXButton;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class WatchlistCell extends ListCell<Movie> {
+    private final Label title = new Label();
+    private final Label detail = new Label();
+    private final Label genres = new Label();
+    private final JFXButton removeBtn = new JFXButton("Remove");
+    private final VBox layout = new VBox(title, detail, genres, removeBtn);
+
+
+    public WatchlistCell(ClickEventHandler<Movie> removeClicked) {
+        super();
+        removeBtn.setOnMouseClicked(mouseEvent -> {
+            removeClicked.onClick(getItem());
+        });
+
+    }
+    @Override
+    protected void updateItem(Movie movie, boolean empty) {
+        super.updateItem(movie, empty);
+
+        if (empty || movie == null) {
+            setText(null);
+        } else {
+            this.getStyleClass().add("movie-cell");
+            title.setText(movie.getTitle());
+            detail.setText(
+                    movie.getDescription() != null
+                            ? movie.getDescription()
+                            : "No description available"
+            );
+
+            genres.setText(movie.getGenres().toString().replace("[","").replace("]",""));
+
+
+
+            // color scheme
+            title.getStyleClass().add("text-yellow");
+            detail.getStyleClass().add("text-white");
+            genres.getStyleClass().add("text-white");
+            layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
+
+            // layout
+            removeBtn.setStyle("-fx-background-color: #f5c518;");
+            title.fontProperty().set(title.getFont().font(20));
+            detail.setMaxWidth(this.getScene().getWidth() - 30);
+            detail.setWrapText(true);
+            layout.setPadding(new Insets(10));
+            layout.spacingProperty().set(10);
+            layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            setGraphic(layout);
+        }
+    }
+}
